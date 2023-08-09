6840fb7f8cfe599cd8fbb4cdb7a130d852340ce0
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-09 22:07:58 +0400

    test

--- src/routes/him/+page.server.js
+++ src/routes/him/+page.server.js
@@ -24,7 +24,7 @@ export async function load() {
 	const { items } = data.essenceCollection;
 
 	// replace \n with <br> tags
-	items[0].himText = items[0].himText.replace(/\n/g, '<br/>');
+	// items[0].himText = items[0].himText.replace(/\n/g, '<br/>');
 
 	return items[0];
 }
