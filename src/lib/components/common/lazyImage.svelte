92f5d0eedba1f9386786778fc3804ae1f10fd366
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-08-16 22:05:23 +0400

    update

--- src/lib/components/common/lazyImage.svelte
+++ src/lib/components/common/lazyImage.svelte
@@ -46,8 +46,8 @@
 
 <style>
   img {
-    opacity: 0;
-    transition: opacity 0.5s ease-in-out;
+    /* opacity: 0;
+    transition: opacity 0.5s ease-in-out; */
     width: 100%;
     height: auto;
     display: block;
